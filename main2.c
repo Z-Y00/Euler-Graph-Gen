800ba76bb38c0440102642a1922a6c863969d180
GengYu Rao <zouyoo@outlook.com>
2019-08-09 17:37:44 -0700

    add buffer

--- main2.c
+++ main2.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]){
   puts(argv[1]);
   puts(argv[2]);
   FILE* input = fopen (argv[1],"r"); 
-  //TODO add auto spliter and auto multi threading
+  //TODO add auto spliter roundrubin and opt
   int output = open (argv[2],O_WRONLY|O_CREAT); 
 
 
@@ -40,11 +40,15 @@ int main(int argc, char *argv[]){
   float feature1 = 0.0;
   int featureInt=0;
   bool lastRun = false;
-  #define PINRT1     write(output,&bufOf1[0],sizeof(int32_t));
-  #define PINRT1f    write(output,&bufOf1f[0],sizeof(float));
-  #define PINRT0     write(output,&zero,sizeof(int32_t));
-  #define ONEHundred &OneHundred
-  #define DEBUG      ;// printf("%d: %d %d\n",a,blockBytes,nodeInfoBytes);//puts("debug");
+  char* write_buf = malloc(SIZE*1000);
+  int write_ptr = 0;
+  #define WRITE(x,y,z) write_buf[write_ptr]=y; write_ptr+=z;
+  #define PINRT1       WRITE(output,&bufOf1[0],sizeof(int32_t));
+  #define PINRT1f      WRITE(output,&bufOf1f[0],sizeof(float));
+  #define PINRT0       WRITE(output,&zero,sizeof(int32_t));
+  #define ONEHundred   &OneHundred
+  #define DEBUG        ;// printf("%d: %d %d\n",a,blockBytes,nodeInfoBytes);//puts("debug");
+
   fscanf(input,"%d	",&next_a);
   while(true){
   //loop begin
@@ -80,50 +84,50 @@ int main(int argc, char *argv[]){
   
   int blockBytes =  4 + (4 + 4 * edge_num)+nodeInfoBytes+edgeInfoBytes*edge_num;
   DEBUG
-  write(output,&blockBytes,sizeof(int32_t));
-  write(output,&nodeInfoBytes,sizeof(int32_t));
-  write(output,&a,sizeof(int64_t));//writer.writeLong(block.getNode_id());
+  WRITE(output,&blockBytes,sizeof(int32_t));
+  WRITE(output,&nodeInfoBytes,sizeof(int32_t));
+  WRITE(output,&a,sizeof(int64_t));//writer.writeLong(block.getNode_id());
   PINRT1 //writer.writeInt(block.getNode_type());
   PINRT1f//writer.writeFloat(block.getNode_weight());
   PINRT1 //writer.writeInt(meta.getEdge_type_num());
   //only have one
-  write(output, &edge_num,sizeof(int32_t));//total num of E
+  WRITE(output, &edge_num,sizeof(int32_t));//total num of E
    //sumWeight should be EdgeNum*1.0
   float sumWeight = edge_num*1.0;
-  write(output,&sumWeight,sizeof(float));
+  WRITE(output,&sumWeight,sizeof(float));
   
-  write(output,edges,sizeof(int64_t)*edge_num);//neighborIdList
-  write(output,bufOf1,sizeof(float)*edge_num);//neighborWeightList as 1
+  WRITE(output,edges,sizeof(int64_t)*edge_num);//neighborIdList
+  WRITE(output,bufOf1,sizeof(float)*edge_num);//neighborWeightList as 1
   
   //no uint64 feature
   PINRT0
   
   //2 float feature getNode_float_feature_num
-  write(output,&two,sizeof(int32_t));
+  WRITE(output,&two,sizeof(int32_t));
   //size of 1st feature
   PINRT1
   //size of 2nd feature
-  write(output,ONEHundred,sizeof(int32_t));//100 for each node
+  WRITE(output,ONEHundred,sizeof(int32_t));//100 for each node
   
   //content of 1st feature
-  write(output,&feature1,sizeof(float));
+  WRITE(output,&feature1,sizeof(float));
   featureInt=(featureInt+1)%2;
   feature1 = featureInt;
   //content of 2nd feature
-  write(output,features,sizeof(float)*100);
+  WRITE(output,features,sizeof(float)*100);
   
   //no binary feature
   PINRT0
   
   //writer.writeInt(block.getEdge().size());
-  write(output,&edge_num,sizeof(int32_t));
+  WRITE(output,&edge_num,sizeof(int32_t));
   
   for(int i=0;i<edge_num;i++){//all the same
-     write(output,&edgeInfoBytes,sizeof(int32_t));
+     WRITE(output,&edgeInfoBytes,sizeof(int32_t));
   }
   for(int i=0;i<edge_num;i++){//for each edge
-     write(output,&a,sizeof(int64_t));//writer.writeLong(edge.getSrc_id());
-     write(output,&edges[i],sizeof(int64_t));//writer.writeLong(edge.getDst_id());
+     WRITE(output,&a,sizeof(int64_t));//writer.writeLong(edge.getSrc_id());
+     WRITE(output,&edges[i],sizeof(int64_t));//writer.writeLong(edge.getDst_id());
      PINRT1
      PINRT1f
      //no edge feature
@@ -133,6 +137,9 @@ int main(int argc, char *argv[]){
   }
   //reset the num
   edge_num = 0;
+  write_ptr = 0;
+  //real write
+  write(output,write_buf,write_ptr);
   if (lastRun) break;
       
 }
